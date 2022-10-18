3afd615e1f84c723fb758bd5b1405951c9b90aa7
Константин Куранов <krot1279@yandex.ru>
2022-10-18 22:02:47 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 def attack(char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
@@ -11,6 +12,7 @@ def attack(char_name, char_class):
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(-3, -1)}')
 
+
 def defence(char_name, char_class):
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
@@ -18,6 +20,7 @@ def defence(char_name, char_class):
         return (f'{char_name} блокировал {10 + randint(-2, 2)} ед. урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} ед. урона')
+
 
 def special(char_name, char_class):
     if char_class == 'warrior':
@@ -27,6 +30,7 @@ def special(char_name, char_class):
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+
 
 def start_training(char_name, char_class):
     if char_class == 'warrior':
@@ -51,6 +55,7 @@ def start_training(char_name, char_class):
             print(special(char_name, char_class))
     return 'Тренировка окончена.'
 
+
 def choice_char_class():
     approve_choice = None
     char_class = None
@@ -71,6 +76,7 @@ def choice_char_class():
                                'или любую другую кнопку, '
                                'чтобы выбрать другого персонажа ').lower()
     return char_class
+
 
 def main():
     print('Приветствую тебя, искатель приключений!')
